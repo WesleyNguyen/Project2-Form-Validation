37cd0fe096cca8e36b601427399d78164776a240
Matthew H. Irby <irby94@uga.edu>
2016-09-12 15:33:23 -0400

    Script + Index

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,9 +1,18 @@
 /*
   	Group Name: The Average Joes
   	Group Members: Bruce Li (Leader)
-  				   Matt Irby
+  				   Matthew H. Irby
   			       Wesley Nguyen
   			       Zachary Gay
     File Name: script.js
-    Purpose: This is the javascript file for index.html
+	Project: Project 2
+    Purpose: This file will act as the JavaScript file for index.html, providing functionality to the page
 */
+
+function submission(){
+	var fs = require("fs");
+var text = fs.readFileSync("../content/ga_zip_codes.txt");
+var textByLine = text.split("\n");
+textByLine.toString();
+document.getElementById("demo").innerHTML = textByLine;
+}
